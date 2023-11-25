771b71f09284ba7224f19a48bec65922201a3ca0
dedinsaid <152006209+zxcdedinsaid@users.noreply.github.com>
2023-11-25 17:08:14 +0500

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Старт называется перед первым обновления кадра
+    void Start()
+    {
+        //Вывод в консоль приветсвие в игру
+        print("здарова");  
+    }
+
+    // обновление называется обновление кадра
+    void Update()
+    {
+        
+    }
+}
